2b1b196dcbd953a0f6ca164e1f9c092f11be5c66
sieutromk <sieutromk@gmail.com>
2019-04-30 02:13:24 +0700

    first commit

---
+++ WebBanSach/WebBanSach/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanSach.Models;
+
+namespace WebBanSach.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbBookDataContext db = new dbBookDataContext();
+
+        // GET: Admin
+        public ActionResult IndexAD()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var un = collection["username"];
+            var pass = collection["pass"];
+            if (String.IsNullOrEmpty(un))
+            {
+                ViewData["Er1"] = "Hãy nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(pass))
+            {
+                ViewData["Er2"] = "Hãy nhập mật khẩu";
+            }
+            else
+            {
+                ADMIN ad = db.ADMINs.SingleOrDefault(n => n.MaAdmin == un && n.Password == pass);
+                if (ad != null)
+                {
+                    Session["admin"] = ad.TenAd;
+                    return RedirectToAction("IndexAD", "Admin");
+                }
+                else
+                    ViewBag.Mess = "Username hoặc Password không đúng";
+            }
+            return View();
+        }
+    }
+}
---
+++ WebBanSach/WebBanSach/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanSach.Models;
+
+namespace WebBanSach.Controllers
+{
+    public class BookController : Controller
+    {
+        dbBookDataContext db = new dbBookDataContext();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            var book = GetBook();
+            return View(book);
+        }
+
+        public List<SACH> GetBook()
+        {
+            return db.SACHes.OrderByDescending(a => a.NgayUpdate).ToList();
+        }
+
+        public ActionResult TheLoai()
+        {
+            var type = from tl in db.THELOAIs select tl;
+            return PartialView(type);
+        }
+
+        public ActionResult SachTheoTL(int id)
+        {
+            var book = from b in db.SACHes where b.MaTL == id select b;
+            return View(book);
+        }
+
+        public ActionResult NXB()
+        {
+            var nxb = from n in db.NXBs select n;
+            return PartialView(nxb);
+        }
+
+        public ActionResult SachTheoNXB(int id)
+        {
+            var book = from b in db.SACHes where b.MaNXB == id select b;
+            return View(book);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var book = from b in db.SACHes
+                       where b.MaSach == id
+                       select b;
+            return View(book.Single());
+        }
+    }
+
+}
